932b5655db370c3c4460061921c074f782c34824
Zoma-glitch <hazem.safwat.55@gmail.com>
2026-09-06 21:38:14 +0300

    Question03

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -20,6 +20,20 @@
             //Why are classes more suitable than structs for large applications?
             //lasses are usually more suitable than structs for large applications because classes are reference types, while structs are value types.
             #endregion
+
+            #region Question03
+            //a) Which class is the parent class?
+            //shipment
+
+            //b) Which class is the child class?
+            //ExpressShipment
+
+            //c) What members are inherited by ExpressShipment?
+            //TrackingCode
+
+            //d) Why is inheritance better than duplicating the same code in multiple classes?
+            //reuse code and avoid duplication
+            #endregion
         }
     }
 }

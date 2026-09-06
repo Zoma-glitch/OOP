303d7f5056fe8523f314ce12e435918021a183e3
Zoma-glitch <hazem.safwat.55@gmail.com>
2026-09-06 21:16:24 +0300

    Question01

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+            //1- What is the difference between a class and a struct?
+            //class                             stuct
+            //reference type                    value type
+
+            //copies the reference              copies the value
+
+            //can be null                       can't be null
+
+            //support iheritance                not support iheritance
+            #endregion
         }
     }
 }

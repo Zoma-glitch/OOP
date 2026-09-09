e3de5f95dd7b008e4bf64388a6dd795d6f470ceb
Zoma-glitch <hazem.safwat.55@gmail.com>
2026-09-09 04:56:39 +0300

    Question02

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -23,6 +23,19 @@
 
             #endregion
 
-        }
+            #region Question02
+            //A)  What is the purpose of the sealed keyword when applied to a class?
+            //sealed reject inheritance from the class
+
+            //B)What is the difference between a sealed class and a sealed method?
+            //sealed class reject inheritance from all class
+
+            //sealed method reject overriding from method
+
+            //C) Can a sealed method be overridden? Why?
+
+            //no becuase the goal of sealed method reject any child class to override from method
+        #endregion
     }
+}
 }

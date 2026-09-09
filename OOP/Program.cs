bab5c63affc2b627c6b956eca55523422c8b11ac
Zoma-glitch <hazem.safwat.55@gmail.com>
2026-09-09 04:22:45 +0300

    Question01

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+
+            //overloading means same + different parameters + static binding + compile time
+
+            //overriding means child redefines Parent method + Dynamic Binding + Runtime
+
+
+
+            #endregion
         }
     }
 }
